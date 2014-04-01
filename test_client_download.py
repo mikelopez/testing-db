a7c7ddfe8bd660f9d99fe422d5b47ff80b3b9d61
Mike Lopez <dev@scidentify.info>
2014-04-01 02:14:00 -0400

    client download sys args

--- test_client_download.py
+++ test_client_download.py
@@ -1,6 +1,5 @@
 from base import *
 
-
 class TestDropboxDownload(unittest.TestCase):
     """Tests the download file process"""
     def setUp(self):
@@ -10,10 +9,18 @@ class TestDropboxDownload(unittest.TestCase):
 
     def test_client_download(self):
         """Tests a download"""
+        try:
+            dlpath, savepath = sys.argv[1], sys.arv[2]
+            termprint('INFO', 'Download Path %s %s' % (dlpath, savepath)
+        except IndexError:
+            dlpath, savepath = "/Test Uploads from API/samplevideo.mp4", \
+                               "samplevideo-download.mp4"
+
+            termprint('ERROR', 'No Params sent, downloading test samplevideo.mp4')
         key, secret, access_token = doauth()
         client = dropbox.client.DropboxClient(access_token)
-        f, metadata = client.get_file_and_metadata('/Test Uploads from API/samplevideo.mp4')
-        out = open('samplevideo-download.mp4', 'wb')
+        f, metadata = client.get_file_and_metadata(dlpath)
+        out = open(savepath, 'wb')
         out.write(f.read())
         out.close()
         termprint("INFO", metadata)
